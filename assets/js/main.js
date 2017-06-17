2a0a6520651cd80b6122839203de8e92ba71dcd9
Zarino Zappia <mail@zarino.co.uk>
2017-06-17 18:36:45 +0100

    Rock Paper Scissors project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
 $(function(){
-
+    $('.project-steps__step').each(function(){
+        var $step = $(this);
+        var $checkbox = $('<span>').addClass('project-steps__step__checkbox');
+        $checkbox.on('click', function(){
+            $step.toggleClass('project-steps__step--completed');
+        });
+        $checkbox.prependTo($step);
+    });
 });
